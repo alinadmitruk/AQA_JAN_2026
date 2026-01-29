01e62f387265f5a38c2bb563ce6172590068a6ba
Alina <alina.dmitruk1997@gmail.com>
2026-01-29 14:18:24 +0100

    Homework5

---
+++ src/main/java/org/prog/session5/CarHW5.java
@@ -0,0 +1,12 @@
+package org.prog.session5;
+
+public class CarHW5 {
+    public String owner;
+
+    public void goTo(String destination, int speed){
+        System.out.println("Car owned by " + owner + " is going to " + destination + " at "+speed +"k/h");
+    }
+
+
+}
+
---
+++ src/main/java/org/prog/session5/HW5.java
@@ -0,0 +1,9 @@
+package org.prog.session5;
+
+public class HW5 {
+    public static void main(String[] args) {
+        CarHW5 Car = new CarHW5();
+       Car.owner = "Alina";
+        Car.goTo( "Kyiv",  75);
+    }
+}
