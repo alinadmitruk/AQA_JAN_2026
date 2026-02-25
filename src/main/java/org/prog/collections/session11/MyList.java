8c0bd5682ec6b1cf921f148b6c2742de9fd7165b
Alina <alina.dmitruk1997@gmail.com>
2026-02-25 23:35:01 +0100

    Homework9

--- src/main/java/org/prog/collections/session11/MyList.java
+++ src/main/java/org/prog/collections/session11/MyList.java
@@ -5,21 +5,46 @@ import java.util.List;
 
 public class MyList {
     public static void main(String[] args){
-        List<> android = new ArrayList<>();
-        List<> apple = new ArrayList<>();
+        List<Android> android = new ArrayList<>();
+        List<Apple> apple = new ArrayList<>();
+
+        android.add(new Android("S32","red"));
+        android.add(new Android("S32","white"));
+        android.add(new Android("S32","black"));
+        android.add(new Android("S32","silver"));
+        android.add(new Android("S32","orange"));
+        android.add(new Android("S32","blue"));
+        android.add(new Android("S32","pink"));
+        android.add(new Android("S32","black"));
+        android.add(new Android("S32","red"));
+        android.add(new Android("S32","silver"));
+
+        apple.add(new Apple("15Pro","black"));
+        apple.add(new Apple("15Pro","gold"));
+        apple.add(new Apple("15Pro","red"));
+        apple.add(new Apple("15Pro","white"));
+        apple.add(new Apple("15Pro","green"));
+        apple.add(new Apple("15Pro","blue"));
+        apple.add(new Apple("15Pro","green"));
+        apple.add(new Apple("15Pro","silver"));
+        apple.add(new Apple("15Pro","yellow"));
+        apple.add(new Apple("15Pro","blue"));
 
 
-
-        boolean result = android.contains("pink");
+        Android pinkAndroid = new Android("S32","pink");
+        boolean result = android.contains(pinkAndroid);
         System.out.println("Android contains pink phone: "+result);
 
-        boolean result1 = android.contains("gold");
+        Android goldAndroid = new Android("S32","gold");
+        boolean result1 = android.contains(goldAndroid);
         System.out.println("Android contains gold phone: "+result1);
 
-        boolean result2 = apple.contains("gold");
-        System.out.println("Apple contains gold phone: "+result2);
+        Apple pinkApple = new Apple("15Pro","pink");
+        boolean result2 = apple.contains(pinkApple);
+        System.out.println("Apple contains pink phone: "+result2);
 
-        boolean result3 = apple.contains("pink");
-        System.out.println("Apple contains pink phone: "+result3);
+        Apple goldApple = new Apple("15Pro","gold");
+        boolean result3 = apple.contains(goldApple);
+        System.out.println("Apple contains gold phone: "+result3);
     }
 }
