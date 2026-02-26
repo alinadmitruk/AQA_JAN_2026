1804630f7e840a430b760242f74552a5ea50ebb7
Alina <alina.dmitruk1997@gmail.com>
2026-02-26 17:57:33 +0100

    Homework9

---
+++ src/main/java/org/prog/collections/session12/MyMain.java
@@ -0,0 +1,11 @@
+package org.prog.collections.session12;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyMain {
+    public static void main (String[] args){
+     Map<Owner, Phone> phoneOwners =new HashMap<>();
+
+    }
+}
---
+++ src/main/java/org/prog/collections/session12/Phone.java
@@ -0,0 +1,6 @@
+package org.prog.collections.session12;
+
+public class Phone {
+    public String model;
+    public String color;
+}
