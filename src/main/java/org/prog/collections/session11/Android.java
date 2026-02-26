cfb120011274a0b3ab43467fc3123d56a50aa9da
Alina <alina.dmitruk1997@gmail.com>
2026-02-26 15:18:43 +0100

    Homework9

---
+++ src/main/java/org/prog/collections/session11/Android.java
@@ -0,0 +1,35 @@
+package org.prog.collections.session11;
+
+public class Android  {
+    public String model;
+    public String color;
+
+    public Android(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Android) {
+            Android other = (Android) obj;
+
+//            if (this.model == null || this.color == null)
+//                throw new MyPhoneException("Model or color is null");
+//
+//
+//            if (other.model == null || other.color == null) {
+//                throw new MyPhoneException("Model or color is null");
+//            }
+            return this.model.equals(other.model) && this.color.equals(other.color);
+        }
+        return false;
+    }
+
+    public int hashCode(){
+//        if (model==null||color==null)
+//            throw new MyPhoneException("Model or color is null");
+        System.out.println();
+        return (this.model + this.color).hashCode();
+    }
+}
